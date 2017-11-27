12c352da4575403acb54c09234d0cd92052402c7
Gott50 <xxGott50xx@gmail.com>
2017-11-27 21:13:48 +0100

    use MessengerExtensions to getUserID

--- src/WebViewer.jsx
+++ src/WebViewer.jsx
@@ -1,33 +1,36 @@
 import React, {Component} from "react";
 import "./webflow.css";
+import MessengerExtensions from "./messengerExtensions";
 
 class WebViewer extends Component {
 
     constructor(props, context) {
         super(props, context);
-        this.state = {
-        }
+        this.state = {}
     }
 
     render() {
         return (
             <div onClick={this.postRequest.bind(this)}>
-                TEST {this.props.location}  {this.props.duration}
+                TEST {this.props.location} {this.props.duration}
             </div>
         );
     }
 
-    postRequest(){
+    postRequest() {
         console.log("postRequest(name)");
-        return fetch('/event', {
-            method: 'post',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            credentials: 'same-origin', // By default, fetch won't send any cookies to the server
-            body: JSON.stringify({name: "fuck"})
-        })
+        return MessengerExtensions.getUserID()
+            .then(userID =>
+                fetch('/event', {
+                    method: 'post',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    credentials: 'same-origin', // By default, fetch won't send any cookies to the server
+                    body: JSON.stringify({userID: userID})
+                })
+            )
     }
 }
 
